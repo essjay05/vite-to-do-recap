c79d49976531ed887af677a58c8222d28db092b4
Joy Serquiña <stepjoyous@gmail.com>
2023-07-09 21:31:17 -0700

    Created handleSubmit function to add newItem to TodoList and display as an li within todoList.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,55 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+  
+  function handleSubmit(e) {
+    e.preventDefault()
+  
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { 
+          id: crypto.randomUUID(), 
+          title: newItem,
+          completed: false
+        }
+      ]
+    })
+
+    setNewItem("")
+  }
+
   return (
     <>
-      <form className="new-item-form">
+      <form className="new-item-form" onSubmit={handleSubmit}>
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
+          <input
+            type="text" 
+            id="item" 
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+          />
         </div>
         <button className="btn" aria-label="Add Item">Add</button>
       </form>
       <h1 className="header">Todo List</h1>
       <ul className="list">
-        <li className="list-item">
-          <label>
-            <input type="checkbox" />
-            Item 1
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li className="list-item">
-          <label>
-            <input type="checkbox" />
-            Item 2
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
+        { todos.map(todo => {
+          return(
+            <li className="list-item"
+             key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed}/>
+                {todo.title}
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          )
+        }) }
       </ul>
     </>
   )
